ab24ab4e7ecdfbf62e35dd68f06381abab593886
Ivan Gerasimov <ivangerasimow@gmail.com>
2026-06-28 15:55:32 +0200

    Update nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,6 +7,8 @@ vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.fillchars = { eob = " " }
+vim.opt.shortmess:append("I")
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
@@ -25,19 +27,24 @@ vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
 -- ============================================================================
 
 -- Save / close
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>W", "<cmd>wa<CR>", { desc = "Save all files" })
+vim.keymap.set("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>S", "<cmd>wa<CR>", { desc = "Save all files" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
 
 -- Buffers
 vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>]", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>[", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>w", "<cmd>bdelete<CR>", { desc = "Close buffer" })
 
 -- Windows
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus upper window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right window" })
+vim.keymap.set("n", "<leader>d", "<cmd>vsplit<CR>", { desc = "Split vertical" })
+vim.keymap.set("n", "<leader>D", "<cmd>split<CR>", { desc = "Split horizontal" })
 
 -- Diagnostics
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
@@ -110,6 +117,12 @@ require("lazy").setup({
   },
 
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    opts = {},
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -151,13 +164,90 @@ require("lazy").setup({
     },
     config = function()
       require("nvim-tree").setup({
+        view = {
+          side = "right",
+        },
         filters = {
           dotfiles = false,
         },
       })
 
       vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
+      vim.keymap.set("n", "<leader>b", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
       vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeFocus<CR>", { desc = "Focus explorer" })
+    end,
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 2,
+      })
+
+      vim.cmd.colorscheme("everforest")
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        markdown = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        yaml = { "prettier" },
+      },
+    },
+    config = function(_, opts)
+      require("conform").setup(opts)
+
+      vim.keymap.set("n", "<leader>cf", function()
+        require("conform").format({
+          async = true,
+          lsp_fallback = true,
+        })
+      end, { desc = "Format file" })
     end,
   },
 }, {
