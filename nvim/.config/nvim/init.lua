5b54ae6776ec9cf697c758aaf0f4df739b87fc54
Ivan Gerasimov <ivangerasimow@gmail.com>
2026-07-03 20:11:31 +0200

    nvim: don‘t ignore case

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,7 +14,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.opt.ignorecase = true
+vim.opt.ignorecase = false
 vim.opt.smartcase = true
 
 vim.opt.clipboard = "unnamedplus"
