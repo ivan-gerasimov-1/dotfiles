03b09cd89a0d2d7a3435afe16adb4515d1a7eee1
Ivan Gerasimov <ivangerasimow@gmail.com>
2026-07-03 20:30:12 +0200

    nvim: move keymaps to a file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,38 +26,7 @@ vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
 -- Keymaps
 -- ============================================================================
 
--- Save / close
-vim.keymap.set("n", "<leader>s", "<cmd>w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>S", "<cmd>wa<CR>", { desc = "Save all files" })
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window" })
-
--- Buffers
-vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<leader>]", "<cmd>bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>[", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<leader>w", "<cmd>bdelete<CR>", { desc = "Close buffer" })
-
--- Windows
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right window" })
-vim.keymap.set("n", "<leader>d", "<cmd>vsplit<CR>", { desc = "Split vertical" })
-vim.keymap.set("n", "<leader>D", "<cmd>split<CR>", { desc = "Split horizontal" })
-
--- Diagnostics
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-
--- LSP
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
-
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
-vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, { desc = "Code action" })
+require("config.keymaps")
 
 -- ============================================================================
 -- lazy.nvim bootstrap
